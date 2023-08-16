d10630919fa115bf3a30aeb81ac8f16c208ed542
Kay Borra <kavyachow@gmail.com>
2023-08-16 17:04:59 -0500

    second commit

--- app/tutorials/urls.py
+++ app/tutorials/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from tutorials import views as tutorials_views
 from django.conf.urls.static import static
 from django.conf import settings
